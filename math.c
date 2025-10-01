71c36a2339653d7e5cf70a2deccefaf213aab1c1
adnan123456a <abdullahaladnan95@gmail.com>
2025-10-01 08:26:19 -0400

    first commit

---
+++ math.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    double x = 9.54;
+    x = pow(x, 2);
+    printf("%.2f\n",x);
+    x = sqrt(9);
+     printf("%.2f\n",x);
+    x = round(3.9);// any relative ...
+      printf("%.2f\n",x);
+     x = floor(3.94);//down
+      printf("%.2f\n",x);
+       x = ceil(3.94);//up
+      printf("%.2f\n",x);
+       x = log(3.5);
+      printf("%.2f\n",x);
+      //and there is up high and down haha
+
+      return 0;
+}
